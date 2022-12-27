4719896eda9bf5f4335aed81d3ff721aa77094aa
Subin Sunilkumar <48793401+subinsunilkumar@users.noreply.github.com>
2022-12-27 15:02:38 +0530

    Updates added

---
+++ PC Cleaner/Cleaner.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace Build
+{
+    partial class Cleaner
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Cleaner));
+            this.button1 = new System.Windows.Forms.Button();
+            this.systemp = new System.Windows.Forms.CheckBox();
+            this.loctemp = new System.Windows.Forms.CheckBox();
+            this.desktop = new System.Windows.Forms.CheckBox();
+            this.document = new System.Windows.Forms.CheckBox();
+            this.download = new System.Windows.Forms.CheckBox();
+            this.Music = new System.Windows.Forms.CheckBox();
+            this.pictures = new System.Windows.Forms.CheckBox();
+            this.videos = new System.Windows.Forms.CheckBox();
+            this.ThreeD = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.recycle = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.checkAllButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Location = new System.Drawing.Point(189, 177);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Clean";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // systemp
+            // 
+            this.systemp.AutoSize = true;
+            this.systemp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(246)))), ((int)(((byte)(244)))));
+            this.systemp.Location = new System.Drawing.Point(35, 43);
+            this.systemp.Name = "systemp";
+            this.systemp.Size = new System.Drawing.Size(85, 17);
+            this.systemp.TabIndex = 2;
+            this.systemp.Text = "Sytem Temp";
+            this.systemp.UseVisualStyleBackColor = false;
+            // 
+            // loctemp
+            // 
+            this.loctemp.AutoSize = true;
+            this.loctemp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(246)))), ((int)(((byte)(244)))));
+            this.loctemp.Location = new System.Drawing.Point(190, 43);
+            this.loctemp.Name = "loctemp";
+            this.loctemp.Size = new System.Drawing.Size(82, 17);
+            this.loctemp.TabIndex = 3;
+            this.loctemp.Text = "Local Temp";
+            this.loctemp.UseVisualStyleBackColor = false;
+            // 
+            // desktop
+            // 
+            this.desktop.AutoSize = true;
+            this.desktop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(246)))), ((int)(((byte)(244)))));
+            this.desktop.Location = new System.Drawing.Point(35, 87);
+            this.desktop.Name = "desktop";
+            this.desktop.Size = new System.Drawing.Size(66, 17);
+            this.desktop.TabIndex = 5;
+            this.desktop.Text = "Desktop";
+            this.desktop.UseVisualStyleBackColor = false;
+            // 
+            // document
+            // 
+            this.document.AutoSize = true;
+            this.document.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(246)))), ((int)(((byte)(244)))));
+            this.document.Location = new System.Drawing.Point(665, 96);
+            this.document.Name = "document";
+            this.document.Size = new System.Drawing.Size(80, 17);
+            this.document.TabIndex = 6;
+            this.document.Text = "Documents";
+            this.document.UseVisualStyleBackColor = false;
+            // 
+            // download
+            // 
+            this.download.AutoSize = true;
+            this.download.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(246)))), ((int)(((byte)(244)))));
+            this.download.Location = new System.Drawing.Point(190, 87);
+            this.download.Name = "download";
+            this.download.Size = new System.Drawing.Size(79, 17);
+            this.download.TabIndex = 7;
+            this.download.Text = "Downloads";
+            this.download.UseVisualStyleBackColor = false;
+            // 
+            // Music
+            // 
+            this.Music.AutoSize = true;
+            this.Music.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(246)))), ((int)(((byte)(244)))));
+            this.Music.Location = new System.Drawing.Point(665, 43);
+            this.Music.Name = "Music";
+            this.Music.Size = new System.Drawing.Size(54, 17);
+            this.Music.TabIndex = 8;
+            this.Music.Text = "Music";
+            this.Music.UseVisualStyleBackColor = false;
+            // 
+            // pictures
+            // 
+            this.pictures.AutoSize = true;
+            this.pictures.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(246)))), ((int)(((byte)(244)))));
+            this.pictures.Location = new System.Drawing.Point(509, 96);
+            this.pictures.Name = "pictures";
+            this.pictures.Size = new System.Drawing.Size(64, 17);
+            this.pictures.TabIndex = 9;
+            this.pictures.Text = "Pictures";
+            this.pictures.UseVisualStyleBackColor = false;
+            // 
+            // videos
+            // 
+            this.videos.AutoSize = true;
+            this.videos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(246)))), ((int)(((byte)(244)))));
+            this.videos.Location = new System.Drawing.Point(35, 131);
+            this.videos.Name = "videos";
+            this.videos.Size = new System.Drawing.Size(58, 17);
+            this.videos.TabIndex = 10;
+            this.videos.Text = "Videos";
+            this.videos.UseVisualStyleBackColor = false;
+            // 
+            // ThreeD
+            // 
+            this.ThreeD.AutoSize = true;
+            this.ThreeD.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(246)))), ((int)(((byte)(244)))));
+            this.ThreeD.Location = new System.Drawing.Point(509, 43);
+            this.ThreeD.Name = "ThreeD";
+            this.ThreeD.Size = new System.Drawing.Size(79, 17);
+            this.ThreeD.TabIndex = 11;
+            this.ThreeD.Text = "3D Objects";
+            this.ThreeD.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(246)))), ((int)(((byte)(244)))));
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(505, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 13;
+            // 
+            // recycle
+            // 
+            this.recycle.AutoSize = true;
+            this.recycle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(246)))), ((int)(((byte)(244)))));
+            this.recycle.Location = new System.Drawing.Point(190, 131);
+            this.recycle.Name = "recycle";
+            this.recycle.Size = new System.Drawing.Size(83, 17);
+            this.recycle.TabIndex = 14;
+            this.recycle.Text = "Recycle Bin";
+            this.recycle.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.Location = new System.Drawing.Point(665, 177);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Uncheck All";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(1, -90);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 603);
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            // 
+            // checkAllButton
+            // 
+            this.checkAllButton.BackColor = System.Drawing.Color.Transparent;
+            this.checkAllButton.Location = new System.Drawing.Point(508, 177);
+            this.checkAllButton.Name = "checkAllButton";
+            this.checkAllButton.Size = new System.Drawing.Size(75, 23);
+            this.checkAllButton.TabIndex = 18;
+            this.checkAllButton.Text = "Check All";
+            this.checkAllButton.UseVisualStyleBackColor = false;
+            this.checkAllButton.Click += new System.EventHandler(this.checkAllButton_Click);
+            // 
+            // Cleaner
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(789, 503);
+            this.Controls.Add(this.checkAllButton);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.recycle);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ThreeD);
+            this.Controls.Add(this.videos);
+            this.Controls.Add(this.pictures);
+            this.Controls.Add(this.Music);
+            this.Controls.Add(this.download);
+            this.Controls.Add(this.document);
+            this.Controls.Add(this.desktop);
+            this.Controls.Add(this.loctemp);
+            this.Controls.Add(this.systemp);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Cleaner";
+            this.Text = "System Cleaner";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox systemp;
+        private System.Windows.Forms.CheckBox loctemp;
+        private System.Windows.Forms.CheckBox desktop;
+        private System.Windows.Forms.CheckBox document;
+        private System.Windows.Forms.CheckBox download;
+        private System.Windows.Forms.CheckBox Music;
+        private System.Windows.Forms.CheckBox pictures;
+        private System.Windows.Forms.CheckBox videos;
+        private System.Windows.Forms.CheckBox ThreeD;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox recycle;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button checkAllButton;
+    }
+}
+
---
+++ PC Cleaner/Cleaner.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Action = System.Action;
+
+
+namespace Build
+{
+    enum RecycleFlag : int
+    {
+        SHERB_NOCONFIRMATION = 0x00000001, // No confirmation, when emptying 
+        SHERB_NOPROGRESSUI = 0x00000001, // No progress tracking window during the emptying of the recycle bin
+        SHERB_NOSOUND = 0x00000004 // No sound whent the emptying of the recycle bin is complete
+    }
+    public partial class Cleaner : Form
+    {
+        [DllImport("Shell32.dll")]
+        static extern int SHEmptyRecycleBin(IntPtr hwnd, string pszRootPath, RecycleFlag dwFlags);
+
+        public Cleaner()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            desktop.Checked = true;
+
+
+            recycle.Checked = true;
+
+            systemp.Checked = true;
+            loctemp.Checked = true;
+
+            ThreeD.Checked = true;
+
+            document.Checked = true;
+            download.Checked = true;
+            Music.Checked = true;
+            pictures.Checked = true;
+            videos.Checked = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            failedTestWorker = new BackgroundWorker();
+            failedTestWorker.DoWork += (obj, ea) => CheckValuesAndClean();
+            failedTestWorker.RunWorkerAsync();
+        }
+
+        private BackgroundWorker failedTestWorker;
+
+        private void CheckValuesAndClean()
+        {
+            var user = Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetParent(Path.GetTempPath()).FullName).FullName).FullName);
+            Invoke(new Action(() => { button1.Text = "Please wait.."; }));
+            Invoke(new Action(() => { button1.Enabled = false; }));
+            if (systemp.Checked)
+            {
+                Invoke(new Action(() => { label1.Text = $"Cleaning {systemp.Text}.."; }));
+                Clean(@"C:\Windows\Temp");
+            }
+            if (loctemp.Checked)
+            {
+                Invoke(new Action(() => { label1.Text = $"Cleaning {loctemp.Text}.."; }));
+                Clean(Path.GetTempPath());
+            }
+            if (ThreeD.Checked)
+            {
+                Invoke(new Action(() => { label1.Text = $"Cleaning {ThreeD.Text}.."; }));
+                Clean($@"{user}\3D Objects");
+            }
+            if (desktop.Checked)
+            {
+                Invoke(new Action(() => { label1.Text = $"Cleaning {desktop.Text}.."; }));
+                Clean($@"{user}\Desktop");
+                
+            }
+            if (document.Checked)
+            {
+                Invoke(new Action(() => { label1.Text = $"Cleaning {document.Text}.."; }));
+                Clean($@"{user}\Documents");
+            }
+            if (download.Checked)
+            {
+                Invoke(new Action(() => { label1.Text = $"Cleaning {download.Text}.."; }));
+                Clean(Path.Combine($@"{user}\Downloads"));
+            }
+            if (Music.Checked)
+            {
+                Invoke(new Action(() => { label1.Text = $"Cleaning {Music.Text}.."; }));
+                Clean($@"{user}\Music");
+            }
+            if (pictures.Checked)
+            {
+                Invoke(new Action(() => { label1.Text = $"Cleaning {pictures.Text}.."; }));
+                Clean($@"{user}\Pictures");
+            }
+            if (videos.Checked)
+            {
+                Invoke(new Action(() => { label1.Text = $"Cleaning {videos.Text}.."; }));
+                Clean($@"{user}\Videos");
+            }
+            if (recycle.Checked)
+            {
+                Invoke(new Action(() => { label1.Text = $"Cleaning {recycle.Text}.."; }));
+                SHEmptyRecycleBin(IntPtr.Zero, null, RecycleFlag.SHERB_NOSOUND | RecycleFlag.SHERB_NOCONFIRMATION);
+            }
+            Invoke(new Action(() => { button1.Text = "Clean"; }));
+            Invoke(new Action(() => { button1.Enabled = true; }));
+            Invoke(new Action(() => { label1.Text = string.Empty; }));
+            MessageBox.Show("Finished Cleaning");
+        }
+        private void Clean(string path)
+        {
+            var command = $"Remove-Item -Recurse -Force \"{path}\\*\"";
+            ExecuteCommandWithAdminPowerShell(command);
+
+
+        }
+
+        public static void ExecuteCommandWithAdminPowerShell(string command)
+        {
+            var psFile = Path.Combine(Environment.CurrentDirectory, "Powershell.ps1");
+            if (File.Exists(psFile))
+            {
+                File.Delete(psFile);
+            }
+            File.WriteAllText(psFile, command);
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo()
+                {
+                    FileName = "powershell.exe",
+                    Arguments = $"-NoProfile -ExecutionPolicy unrestricted -file \"{psFile}\"",
+                    UseShellExecute = false,
+                    WindowStyle = ProcessWindowStyle.Hidden,
+                    Verb = "runas",
+                    CreateNoWindow = true
+                }
+            };
+            process.Start();
+            process.WaitForExit();
+            if (File.Exists(psFile))
+            {
+                File.Delete(psFile);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            recycle.Checked = false;
+            desktop.Checked = false;
+            systemp.Checked = false;
+            loctemp.Checked = false;
+
+            ThreeD.Checked = false;
+
+            document.Checked = false;
+            download.Checked = false;
+            Music.Checked = false;
+            pictures.Checked = false;
+            videos.Checked = false;
+        }
+
+        private void checkAllButton_Click(object sender, EventArgs e)
+        {
+            recycle.Checked = true;
+            desktop.Checked = true;
+            systemp.Checked = true;
+            loctemp.Checked = true;
+
+            ThreeD.Checked = true;
+
+            document.Checked = true;
+            download.Checked = true;
+            Music.Checked = true;
+            pictures.Checked = true;
+            videos.Checked = true;
+        }
+    }
+}
+
+
